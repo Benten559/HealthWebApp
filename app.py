da6aa075ec35b67e5020fbc732f4d1fe2f173359
Benten559 <37288756+Benten559@users.noreply.github.com>
2020-12-06 00:01:05 -0800

    configuring feature directory
    updated the path for ML model

--- app.py
+++ app.py
@@ -18,13 +18,6 @@ img_trans = transforms.Compose([transforms.Resize((128, 128)),
                                  transforms.CenterCrop(100),
 #                                 transforms.ToTensor(),
 ])
-app = Flask(__name__,static_folder='static/')
-app.config['SECRET_KEY'] = '78sOME098random987key10847'
-# def checkFile():
-#   path = "G:\Shared drives\CS150Project\CSCI150 files\model data"
-
-
-
 
 app = Flask(__name__,static_folder='static/')
 app.config['SECRET_KEY'] = '78sOME098random987key10847'
@@ -156,7 +149,7 @@ def identify():
   test_sim = test_output[0]
 
   # search/compare
-  path = r"Machine learning model\datasets\CSCI150 files\model data"
+  path = r"Machine learning model\datasets\feats\CSCI150 files\model data"
   resultmain = 0
   maxi= float(0) 
   for i in range (0, 1153):
@@ -169,7 +162,7 @@ def identify():
       resultmain = i
       
   # CHECK THE FEATURE VECTORS
-  root_dir = r"cropped"
+  root_dir = r"Machine learning model\datasets\cropped"
 
   k = 0
   dataset = pd.read_csv("templates/table.csv")
